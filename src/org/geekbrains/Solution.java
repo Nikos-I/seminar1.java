3dbee2c5f61e2feee5ea7fc2185457bc59e74b44
Nikos-I <109852894+Nikos-I@users.noreply.github.com>
2022-12-01 15:10:33 +0300

    Семинар 1 ДЗ1 Завершено

--- src/org/geekbrains/Solution.java
+++ src/org/geekbrains/Solution.java
@@ -2,9 +2,7 @@ package org.geekbrains;
 
 public class Solution {
     public int maxAreaOfIsland(int[][] grid) {
-
         int maxArea = 0;
-
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
                 if (grid[i][j] == 1) {
@@ -14,21 +12,18 @@ public class Solution {
                     }
                 }
             }
-        } return maxArea;
+        }
+        return maxArea;
     }
 
     public int dfs(int[][] grid, int i, int j) {
-
         int curArea;
-
         if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0) {
             return 0;
-        }
-        else {
+        } else {
             grid[i][j] = 0;
-            curArea = (1 + dfs(grid, i-1, j) + dfs(grid, i+1, j) + dfs(grid, i, j-1) + dfs(grid, i, j+1));
+            curArea = (1 + dfs(grid, i - 1, j) + dfs(grid, i + 1, j) + dfs(grid, i, j - 1) + dfs(grid, i, j + 1));
             return curArea;
         }
     }
-
 }
